5b2a4d714e3e71db414814da78eb9c5f3f738bc0
Shayan <shayan.delbari@protonmail.com>
2024-12-22 17:15:12 -0500

    add error handling component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="flex h-[90vh] w-screen flex-col items-center justify-center">
+	<h1 class="m-3 text-8xl font-thin text-gray-600">{page.status}</h1>
+	<p>{page.error?.message}</p>
+</div>
