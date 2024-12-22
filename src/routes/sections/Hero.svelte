b70739d7beb844ce354eeeb716b612863bc7b577
Shayan <shayan.delbari@protonmail.com>
2024-12-22 17:15:44 -0500

    feat: add enhanced image support and update Hero section layout - NOT COMPLETE

--- src/routes/sections/Hero.svelte
+++ src/routes/sections/Hero.svelte
@@ -1,5 +1,21 @@
-<div class="h-screen">
-	<h1 class="my-auto text-pretty font-mono text-5xl font-black">
-		Hi there, <br />my name is Shayan
-	</h1>
+<script lang="ts">
+	import image from '$lib/images/Shayan.png?enhanced';
+</script>
+
+<div class="flex min-h-[90vh]">
+	<div class="mx-[6vh] flex w-full flex-row justify-between">
+		<div class="my-auto">
+			<h1 class="font-mono text-5xl font-black">
+				Hi there,<br />my name is Shayan
+			</h1>
+			<p class="mt-5 max-w-prose">
+				I have a passion for creating beautiful and functional designs, both in code and graphics.
+				With a background in both development and design, I bring a unique perspective to every
+				project I work on.
+			</p>
+		</div>
+		<div class="my-auto">
+			<enhanced:img src={image} alt="An alt text" class="my-auto h-fit w-[40vh]" />
+		</div>
+	</div>
 </div>
