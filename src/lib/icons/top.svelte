b03afbd6bfd5e953cbd9aba101beda44d9e09c35
shayandelbari <shayan.delbari@protonmail.com>
2024-12-25 18:04:28 -0500

    changed icons to use heroicon

--- src/lib/icons/top.svelte
+++ src/lib/icons/top.svelte
@@ -3,12 +3,11 @@
 </script>
 
 <svg
-	width="24px"
-	height="24px"
-	viewBox="0 0 24 24"
-	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
-	class={classList}
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="2"
+	class="size-6 {classList}"
 >
-	<path d="M17 15L12 10L7 15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+	<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
 </svg>
