d88d93afeffca052f6e3211308eb35f9d731b91c
shayandelbari <shayan.delbari@protonmail.com>
2025-01-29 12:42:17 -0500

    fixed a layout issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,28 +16,28 @@
 	<Hero />
 </section>
 
-<section id="about">
+<section id="about" class="mt-20 lg:mt-40">
 	<About />
 </section>
 
-<section id="experience" class="mt-40">
+<section id="experience" class="mt-20 lg:mt-40">
 	<Experience />
 </section>
 
-<section id="skills" class="mt-40">
+<section id="skills" class="mt-20 lg:mt-40">
 	<Skills />
 </section>
 
-<section id="projects" class="mt-40">
+<section id="projects" class="mt-20 lg:mt-40">
 	<Projects />
 </section>
 
-<section id="contact" class="mt-40">
+<section id="contact" class="mt-20 lg:mt-40">
 	<Contact />
 </section>
 
 <style>
 	section {
-		@apply px-40;
+		@apply px-5 md:px-20 lg:px-40;
 	}
 </style>
